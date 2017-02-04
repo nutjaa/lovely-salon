d4c319011cdce6485dbfa085d3fe013cb45b72f0
Nut Chanyong <nutjaa@msn.com>
2017-02-04 21:05:17 +0700

    add daily jobs table

---
+++ database/migrations/2017_02_04_140007_create_daily_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDailyJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('daily_jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->integer('task_id');
+            $table->text('description');
+            $table->boolean('is_loyal_customer');
+            $table->integer('customer_id');
+            $table->decimal('amount', 5, 2);
+            $table->dateTime('task_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('daily_jobs');
+    }
+}
